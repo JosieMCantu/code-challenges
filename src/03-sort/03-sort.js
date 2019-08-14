f4901fe9e1680867de96913ed42bca3bec474dbe
martypdx <marty@martypdx.com>
2019-08-14 10:25:15 -0700

    code challenge 03 setup

---
+++ src/03-sort/03-sort.js
@@ -0,0 +1,148 @@
+/* eslint-disable no-unused-vars */
+
+/* ------------------------------------------------------------------------------------------------
+CHALLENGE 1
+
+Write a function named sortBackwards that takes in an array of numbers and returns the same array, with the numbers sorted, highest to smallest.
+------------------------------------------------------------------------------------------------ */
+
+export const sortBackwards = (arr) => {
+  // Solution code here...
+};
+
+/* ------------------------------------------------------------------------------------------------
+CHALLENGE 2
+
+Write a function named alphabetize that takes in an array of strings and returns the same array with the strings sorted alphabetically.
+
+In this alphabetization, capital letters come before lower case letters.
+
+For example, ['Alphabet', 'Zebra', 'alphabet', 'carrot'] is correctly sorted.
+------------------------------------------------------------------------------------------------ */
+
+export const alphabetize = (arr) => {
+  // Solution code here...
+};
+
+/* ------------------------------------------------------------------------------------------------
+CHALLENGE 3
+
+Write a function named sortByLength that takes in an array of strings and returns the same array, with the strings sorted by their length, lowest to highest.
+------------------------------------------------------------------------------------------------ */
+
+export const sortByLength = (arr) => {
+  // Solution code here...
+};
+
+/* ------------------------------------------------------------------------------------------------
+CHALLENGE 4
+
+Write a function named alphabetizeBetter that takes in an array of strings and returns the same array, with the strings sorted alphabetically. Capitalization should not change the sort order of two strings.
+
+For example, ['Alphabet', 'alphabet', 'carrot', 'Zebra'] is correctly sorted, and so is ['alphabet', 'Alphabet', 'carrot', 'Zebra'].
+------------------------------------------------------------------------------------------------ */
+
+export const alphabetizeBetter = (arr) => {
+  // Solution code here...
+};
+
+/* ------------------------------------------------------------------------------------------------
+CHALLENGE 5
+
+Write a function named sortByPrice that takes in an array of objects, each of which has a 'price' property, and sorts those objects by price, lowest to highest, returning the same array.
+
+Here is an example of the input:
+[
+  {name: 'Sweatshirt', price: 45},
+  {name: 'Bookmark', price: 2.50},
+  {name: 'Tote bag', price: 15}
+];
+------------------------------------------------------------------------------------------------ */
+
+export const sortByPrice = (arr) => {
+  // Solution code here...
+};
+
+/* ------------------------------------------------------------------------------------------------
+CHALLENGE 6
+
+Write a function named sortNumbersByLength that takes in an array of numbers and sorts those numbers by their length.
+
+For example, [1, 14, 0.2, -281, 54782] is only correctly sorted in that order.
+------------------------------------------------------------------------------------------------ */
+
+export const sortNumbersByLength = (arr) => {
+  // Solution code here...
+};
+
+/*-----------------------------------------------------------------------------------------------
+CHALLENGE 7
+
+Write a function named sortPeople that takes in an array of Person objects, each of which has firstName, lastName, and age properties, and sorts those people by their last names. Do not worry about capitalization or first names.
+------------------------------------------------------------------------------------------------ */
+
+export class Person {
+    constructor(firstName, lastName, age) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.age = age;
+    }
+}
+
+export const people = [
+    new Person('Wes', 'Washington', 25),
+    new Person('Casey', 'Codefellow', 38),
+    new Person('Stan', 'Seattle', 67),
+];
+
+export const sortPeople = (arr) => {
+  // Solution code here...
+};
+
+/* ------------------------------------------------------------------------------------------------
+CHALLENGE 8
+
+Write a function named sortPeopleBetter that takes in an array of Person objects, each of which has firstName, lastName, and age properties, and sorts those people by their last names.
+
+If two people share the same last name, alphabetize on their first name.
+
+If two people have the same full name, the younger one should come first. Do not worry about capitalization.
+------------------------------------------------------------------------------------------------ */
+
+export const sortPeopleBetter = (arr) => {
+  // Solution code here...
+};
+
+/* ------------------------------------------------------------------------------------------------
+CHALLENGE 9 - Stretch Goal
+
+Write a function named sortMeetingsByDay that takes in an array of objects, each of which represents a meeting happening a particular day of the week, with a particular start time and end time.
+
+Sort the meetings by the day on which they happen, Monday-Friday. It does not matter which order meetings come in on a particular day. For example, if there are two meetings on Monday, it does not matter which comes first.
+------------------------------------------------------------------------------------------------ */
+
+export class Meeting {
+    constructor(dayOfWeek, start, end) {
+        this.dayOfWeek = dayOfWeek;
+        this.start = start;
+        this.end = end;
+    }
+}
+
+export const sortMeetingsByDay = (arr) => {
+  // Solution code here...
+};
+
+/* ------------------------------------------------------------------------------------------------
+CHALLENGE 10 - Stretch Goal
+
+This challenge should use the array of meetings from challenge 9, above.
+
+Sort the meetings in the order that they start. If two meetings start at the same time on the same day, the shorter meeting should come first.
+
+You DO NOT need to use your solution to Challenge 9 in completing Challenge 10.
+------------------------------------------------------------------------------------------------ */
+
+export const sortSchedule = (arr) => {
+  // Solution code here...
+};
