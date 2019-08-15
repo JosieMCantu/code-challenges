32d6919b2c07e5c269d973aefc7069673779fdaa
martypdx <marty@martypdx.com>
2019-08-15 09:42:19 -0700

    remove problem with non-deterministic sort

--- src/03-sort/03-sort.js
+++ src/03-sort/03-sort.js
@@ -108,27 +108,7 @@ export const sortPeopleBetter = (arr) => {
 };
 
 /* ------------------------------------------------------------------------------------------------
-CHALLENGE 9 - Stretch Goal
-
-Write a function named sortMeetingsByDay that takes in an array of objects, each of which represents a meeting happening a particular day of the week, with a particular start time and end time.
-
-Sort the meetings by the day on which they happen, Monday-Friday. It does not matter which order meetings come in on a particular day. For example, if there are two meetings on Monday, it does not matter which comes first.
------------------------------------------------------------------------------------------------- */
-
-export class Meeting {
-    constructor(dayOfWeek, start, end) {
-        this.dayOfWeek = dayOfWeek;
-        this.start = start;
-        this.end = end;
-    }
-}
-
-export const sortMeetingsByDay = (arr) => {
-  // Solution code here...
-};
-
-/* ------------------------------------------------------------------------------------------------
-CHALLENGE 10 - Stretch Goal
+CHALLENGE 09 - Stretch Goal
 
 This challenge should use the array of meetings from challenge 9, above.
 
