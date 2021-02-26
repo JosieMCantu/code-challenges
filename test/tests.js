d734a72f9397f6a0d1c768b5113c939a37177fd2
harriet-the-spy <dpcairns@gmail.com>
2021-02-25 16:47:57 -0800

    fix merge

--- test/tests.js
+++ test/tests.js
@@ -10,7 +10,6 @@ import './09-object-iteration.test.js';
 import './10-chaining.test.js';
 import './11-regex-part-2.test.js';
 import './12-nested-loops.test.js';
-=======
 import './13-strings.test.js';
 import './14-review.test.js';
 
