7d666c1a9cdc192b71352a8a00745d485edc3601
harriet-the-spy <dpcairns@gmail.com>
2021-02-25 17:08:55 -0800

    add comments

--- test/tests.js
+++ test/tests.js
@@ -1,17 +1,17 @@
 import './01-for-each.test.js';
-import './02-by-val-by-ref.test.js';
-import './03-sort.test.js';
-import './04-regex-part-1.test.js';
-import './05-split-join-slice-splice.test.js';
-import './06-map.test.js';
-import './07-filter.test.js';
-import './08-reduce.test.js';
-import './09-object-iteration.test.js';
-import './10-chaining.test.js';
-import './11-regex-part-2.test.js';
-import './12-nested-loops.test.js';
-import './13-strings.test.js';
-import './14-review.test.js';
+// import './02-by-val-by-ref.test.js';
+// import './03-sort.test.js';
+// import './04-regex-part-1.test.js';
+// import './05-split-join-slice-splice.test.js';
+// import './06-map.test.js';
+// import './07-filter.test.js';
+// import './08-reduce.test.js';
+// import './09-object-iteration.test.js';
+// import './10-chaining.test.js';
+// import './11-regex-part-2.test.js';
+// import './12-nested-loops.test.js';
+// import './13-strings.test.js';
+// import './14-review.test.js';
 
 const test = QUnit.test;
 
